b4795d4c09661d15f16148adbfd8fb25401b6243
studente <studente@US-Lab-19.local>
2019-04-17 12:24:38 -0400

    po

---
+++ barnga 2/public/javascripts/index.js
@@ -0,0 +1,11 @@
+(() => {
+  var socket = io()
+
+  socket.on('broadcast', function(message) {
+    console.log(message);
+  })
+
+  window.broadcast = function() {
+    socket.emit('broadcast', 'howdy')
+  }
+})();
